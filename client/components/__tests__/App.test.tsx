e654c5055c8853933ec41a22a426f8e941a10dfd
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-04-23 20:18:14 +1200

    remove data-testid

--- client/components/__tests__/App.test.tsx
+++ client/components/__tests__/App.test.tsx
@@ -3,8 +3,8 @@ import { render } from "@testing-library/react"
 import App from "../App"
 
 test("should render App with correct text", () => {
-  const { getByTestId } = render(<App />)
-  const header = getByTestId("appHeader")
+  const { getByRole } = render(<App />)
+  const header = getByRole("heading")
 
   expect(header.textContent).toBe("App has arrived")
 })
