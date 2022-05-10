687c710c0fbfb90b126450df20bb3664fa9fbbcb
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-05-10 12:28:54 +1200

    add JSONRecipe type

---
+++ server/types/JSONRecipe.d.ts
@@ -0,0 +1,24 @@
+interface QuantityUnit {
+  quantity: number;
+  unit: string;
+}
+
+interface Ingredient {
+  ingredientId?: number;
+  ingredient: string;
+  quantity: QuantityUnit;
+}
+
+interface Instruction {
+  stepNumber: number;
+  instructions: string;
+}
+
+export interface IncomingJSONRecipe {
+  name: string;
+  cookingTime: QuantityUnit;
+  prepTime: QuantityUnit;
+  instructions: Instruction[];
+  ingredients: Ingredient[];
+  notes: string;
+}
