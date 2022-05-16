70750c51331da06bf005471b352d331149555869
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-05-16 16:06:54 +1200

    add universal queries

---
+++ server/db/functions/basicCrud.ts
@@ -0,0 +1,72 @@
+import { DeletionDBError, GetDBError } from '../../errors/crudDBErrors'
+import { IngredientsDatabase, RecipeDatabase, RecipeIngredientDatabase, UserRecipeDatabase } from '../../types/DatabaseObjects'
+import connection from '../connection'
+
+type Table =  'ingredients' | 'recipe_ingredients' | 'recipes' |'user_recipes'
+type Item = Partial<IngredientsDatabase | RecipeIngredientDatabase | RecipeDatabase | UserRecipeDatabase>
+
+interface Selector {
+  // as in { 'userId': userId }
+  [x: string]: number | string
+}
+
+export function addItemToDatabase (table: Table, item: Item, db = connection): Promise<number> {
+  return db(table)
+    .insert(item)
+    .then(id => id[0])
+}
+
+export function getItemsBySelector (table: Table, selector: Selector, db = connection) {
+  return db(table)
+    .select()
+    .where(selector)
+    .then((results) => {
+      if(results.length === 0) {
+        throw new GetDBError(table)
+      }
+      return results
+    })
+}
+
+export function getFirstItemBySelector (table: Table, selector: Selector, db = connection) {
+  return db(table)
+    .select()
+    .where(selector)
+    .first()
+    .then((result) => {
+      if(result) {
+        return result
+      } else {
+        throw new GetDBError(table)
+      }
+    })
+}
+
+export function getIdByUniqueProperty (table: Table, property: Selector, db = connection) {
+  console.log(property)
+  return db(table)
+    .select()
+    .where(property)
+    .first()
+    .then((result: Item | undefined) => {
+      console.log('results')
+      if(result?.id) {
+        return result?.id
+      } else {
+        throw new GetDBError(table)
+      }
+    })
+}
+
+export function deleteItemBySelector (table: Table, selector: Selector, db = connection) {
+  return db(table)
+    .where(selector)
+    .del()
+    .then(rowsDeleted => {
+      if(rowsDeleted) {
+        return rowsDeleted
+      } else {
+        throw new DeletionDBError(table)
+      }
+    })
+}
