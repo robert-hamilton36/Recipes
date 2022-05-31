7bed01bf86835ec85bcbef3de5b497ef494f206d
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-05-31 16:36:41 +1200

    remove unneeded unique

--- server/db/migrations/20220515041043_user_recipes.ts
+++ server/db/migrations/20220515041043_user_recipes.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 
 export async function up (knex: Knex): Promise<void> {
   return knex.schema.createTable('user_recipes', table => {
-    table.increments('id').unique()
+    table.increments('id')
     table.integer('user_id')
     table.integer('recipe_id')
   })
