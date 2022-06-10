7d06a5cfdac96fb3acec848c1747b988b635341e
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-06-10 18:12:41 +1200

    add empty body catch

--- server/controllers/recipe.ts
+++ server/controllers/recipe.ts
@@ -7,65 +7,91 @@ import { handleGetUpdateDeleteRecipes } from "../functions/errorHandlers";
 
 import { JSONRecipe } from "../types/JSONRecipe";
 
-export const addNewRecipe = async (req: Request, res: Response) => {
-  
+export const addNewRecipe = async (req: Request, res: Response) => { 
   try {
     const jsonRecipe: JSONRecipe = req.body.recipe
     const userId = req.body.userId
+  
+    if (userId === undefined) { 
+      return res.status(400).json({error: 'No userId'}) 
+    }
+
+    if(jsonRecipe === undefined) {
+      return res.status(400).json({error: 'No jsonRecipe'}) 
+    }
+
+    // add recipe and then add ingredients if needed
     const recipeId = await addRecipe(jsonRecipe)
+
+    // add userRecipe join
     await userSavesRecipe(userId, recipeId)
 
-    res.status(201).json({ id: recipeId })
+    return res.status(201).json({ id: recipeId })
   } catch (e: unknown) {
-    res.status(500).json({error: 'Something went wrong'})
+    return res.status(500).json({error: 'Something went wrong'})
   }
+  
 }
 
 export const saveRecipe = async (req: Request, res: Response) => {
-  const recipeId = parseInt(req.params.recipeId)
-  const userId: number = req.body.userId
-
   try {
+    const recipeId = parseInt(req.params.recipeId)
+    const userId: number = req.body.userId
+  
+    if (userId === undefined) { 
+      return res.status(400).json({error: 'No userId'}) 
+    } 
+
     const id = await userSavesRecipe(userId, recipeId)
 
-    res.status(201).json({id})
+    return res.status(201).json({id})
   } catch (e: unknown) {
-    res.status(500).json({error: 'Something went wrong'})
+   return res.status(500).json({error: 'Something went wrong'})
   }
 }
 
 export const getUserRecipes = async (req: Request, res: Response) => {
   try {
     const userId = parseInt(req.params.userId)
+
     const recipes = await getUsersRecipesByUserId(userId)
-    res.json({ recipes })
+
+    return res.json({ recipes })
   } catch(e) {
     const {code, error} = handleGetUpdateDeleteRecipes(e)
-    res.status(code).json({ error })
+    return res.status(code).json({ error })
   }
 }
 
 export const getRecipe = async (req: Request, res: Response) => {
   try {
     const recipeId = parseInt(req.params.recipeId)
+
     const recipe = await getRecipeById(recipeId)
-    res.json({ recipe })
+
+    return res.json({ recipe })
   } catch(e) {
     const {code, error} = handleGetUpdateDeleteRecipes(e)
-    res.status(code).json({ error })
+    return res.status(code).json({ error })
   }
 }
 
-export const editRecipe = async (req: Request, res: Response) => {
+export const editRecipe = async (req: Request, res: Response) => {  
   try {
     const recipeId = parseInt(req.params.recipeId)
-    const editedRecipe = req.body.edit as Partial<JSONRecipe>
+    const editedRecipe = req.body.editedRecipe as Partial<JSONRecipe>
+  
+    if (editedRecipe === undefined) { 
+      return res.status(400).json({error: 'No edited recipe'}) 
+    }
 
+    // update the recipe
     await updateRecipe(recipeId, editedRecipe)
-    res.json({ edited: true})
+
+    return res.json({ edited: true})
   } catch(e: unknown) {
     const {code, error} = handleGetUpdateDeleteRecipes(e)
-    res.status(code).json({ edited: false, error })
+    return res.status(code).json({ edited: false, error })
   }
 }
 
@@ -73,12 +99,17 @@ export const unsaveRecipe = async (req: Request, res: Response) => {
   try {
     const recipe_id = parseInt(req.params.recipeId)
     const user_id = req.body.userId
+
+    if (user_id === undefined) { 
+      return res.status(400).json({error: 'No userId'}) 
+    }
+
     // delete the recipe user connection for given user
     await deleteItemBySelector('user_recipes', {user_id, recipe_id})
     
-    res.json({unsaved: true})
+    return res.json({unsaved: true})
   } catch(e) {
     const {code, error} = handleGetUpdateDeleteRecipes(e)
-    res.status(code).json({ error })
+    return res.status(code).json({ error })
   }
 }
--- server/routes/__tests__/recipe.test.ts
+++ server/routes/__tests__/recipe.test.ts
@@ -33,6 +33,20 @@ describe('POST /add', () => {
       MockedAddRecipe.mockResolvedValue(1)
       MockedUserSavesRecipe.mockResolvedValue(1)
     })
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    test('Functions are called correctly', async () => {
+      await request(mockedServer).post('/add').send({userId: 1, recipe: CaramelSliceJSONRecipe})
+      expect(MockedAddRecipe).toHaveBeenCalledTimes(1)
+      expect(MockedAddRecipe.mock.calls[0][0]).toEqual(CaramelSliceJSONRecipe)
+      expect(MockedUserSavesRecipe).toHaveBeenCalledTimes(1)
+      expect(MockedUserSavesRecipe.mock.calls[0][0]).toBe(1)
+      expect(MockedUserSavesRecipe.mock.calls[0][1]).toBe(1)
+    })
+
     test('returns status code 200', async () => {
       const response = await request(mockedServer).post('/add').send({userId: 1, recipe: CaramelSliceJSONRecipe})
       expect(response.statusCode).toBe(201)
@@ -46,6 +60,72 @@ describe('POST /add', () => {
     test('body contains id', async () => {
       const response = await request(mockedServer).post('/add').send({userId: 1, recipe: CaramelSliceJSONRecipe})
       expect(response.body.id).toBe(1)
+    })
+  })
+
+  describe('invalid request data', () => {
+    describe('no bodyy.userId', () => {
+      beforeAll(() => {
+        MockedAddRecipe.mockResolvedValue(1)
+        MockedUserSavesRecipe.mockResolvedValue(1)
+      })
+
+      beforeEach(() => {
+        jest.clearAllMocks()
+      })
+
+      test('Functions are not called', async () => {
+        await request(mockedServer).post('/add').send({recipe: CaramelSliceJSONRecipe})
+        expect(MockedAddRecipe).toHaveBeenCalledTimes(0)
+        expect(MockedUserSavesRecipe).toHaveBeenCalledTimes(0)
+      })
+
+      test('returns status code 200', async () => {
+        const response = await request(mockedServer).post('/add').send({recipe: CaramelSliceJSONRecipe})
+        expect(response.statusCode).toBe(400)
+      })
+  
+      test('header specifies content type json', async () => {
+        const response = await request(mockedServer).post('/add').send({recipe: CaramelSliceJSONRecipe})
+        expect(response.headers['content-type']).toContain('json')
+      })
+    
+      test('body contains id', async () => {
+        const response = await request(mockedServer).post('/add').send({recipe: CaramelSliceJSONRecipe})
+        expect(response.body.error).toBe('No userId')
+      })
+    })
+
+    describe('no body.recipe', () => {
+      beforeAll(() => {
+        MockedAddRecipe.mockResolvedValue(1)
+        MockedUserSavesRecipe.mockResolvedValue(1)
+      })
+
+      beforeEach(() => {
+        jest.clearAllMocks()
+      })
+
+      test('Functions are not called', async () => {
+        await request(mockedServer).post('/add').send({userId: 1})
+        expect(MockedAddRecipe).toHaveBeenCalledTimes(0)
+        expect(MockedUserSavesRecipe).toHaveBeenCalledTimes(0)
+      })
+
+      test('returns status code 200', async () => {
+        const response = await request(mockedServer).post('/add').send({userId: 1})
+        expect(response.statusCode).toBe(400)
+      })
+  
+      test('header specifies content type json', async () => {
+        const response = await request(mockedServer).post('/add').send({userId: 1})
+        expect(response.headers['content-type']).toContain('json')
+      })
+    
+      test('body contains id', async () => {
+        const response = await request(mockedServer).post('/add').send({userId: 1})
+        expect(response.body.error).toBe('No jsonRecipe')
+      })
     })
   })
 
@@ -69,6 +149,18 @@ describe('POST /save/:recipeId', () => {
     beforeAll(() => {
       MockedUserSavesRecipe.mockResolvedValue(1)
     })
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    test('Functions are called correctly', async () => {
+      await request(mockedServer).post('/save/1').send({ userId: 1 })
+      expect(MockedUserSavesRecipe).toHaveBeenCalledTimes(1)
+      expect(MockedUserSavesRecipe.mock.calls[0][0]).toBe(1)
+      expect(MockedUserSavesRecipe.mock.calls[0][1]).toBe(1)
+    })
+
     test('returns status code 200', async () => {
       const response = await request(mockedServer).post('/save/1').send({ userId: 1 })
       expect(response.statusCode).toBe(201)
@@ -85,12 +177,46 @@ describe('POST /save/:recipeId', () => {
     })
   })
 
+  describe('invalid request data', () => {
+    describe('no body.userId', () => {
+
+      beforeAll(() => {
+        MockedUserSavesRecipe.mockResolvedValue(1)
+      })
+  
+      beforeEach(() => {
+        jest.clearAllMocks()
+      })
+
+      test('Functions are not called', async () => {
+        await request(mockedServer).post('/save/1').send()
+        expect(MockedUserSavesRecipe).toHaveBeenCalledTimes(0)
+      })
+
+      test('returns status code 400', async () => {
+        const response = await request(mockedServer).post('/save/1').send()
+        expect(response.statusCode).toBe(400)
+      })
+  
+      test('header specifies content type json', async () => {
+        const response = await request(mockedServer).post('/save/1').send()
+        expect(response.headers['content-type']).toContain('json')
+      })
+    
+      test('body contains error', async () => {
+        const response = await request(mockedServer).post('/save/1').send()
+        expect(response.body.error).toBe('No userId')
+      })
+    })
+  })
+
   describe('generic errors', () => {
     test('addRecipe() throws', async () => {
       MockedUserSavesRecipe.mockRejectedValueOnce(new Error('Transaction failed'))
       const response = await request(mockedServer).post('/save/1').send({ userId: 1 })
       expect(response.statusCode).toBe(500)
     })
+    
   })
 })
 
@@ -98,6 +224,16 @@ describe('GET /getUserRecipes/:userId', () => {
   describe('valid request data', () => {
     beforeAll(() => {
       MockedGetUsersRecipesByUserId.mockResolvedValue([CaramelSliceJSONRecipeWithIds])
+    })
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    test('Functions are called correctly', async () => {
+      await request(mockedServer).get('/getUserRecipes/1')
+      expect(MockedGetUsersRecipesByUserId).toHaveBeenCalledTimes(1)
+      expect(MockedGetUsersRecipesByUserId.mock.calls[0][0]).toBe(1)
     })
 
     test('returns status code 200', async () => {
@@ -118,22 +254,33 @@ describe('GET /getUserRecipes/:userId', () => {
   })
 
   describe('invalid request data', () => {
+    
     test('returns status code 400 with error message Wrong id', async () => {
+      jest.clearAllMocks()
+
       MockedGetUsersRecipesByUserId.mockRejectedValueOnce(new GetDBError('recipes'))
       MockedHandleGetUpdateDeleteRecipes.mockReturnValueOnce({ code: 400, error: `GetError: Wrong 'recipes' id`})
+
       const response = await request(mockedServer).get('/getUserRecipes/1')
       expect(response.statusCode).toBe(400)
       expect(response.body.error).toBe(`GetError: Wrong 'recipes' id`)
+      expect(MockedGetUsersRecipesByUserId).toHaveBeenCalledTimes(1)
+      expect(MockedHandleGetUpdateDeleteRecipes).toHaveBeenCalledTimes(1)
     })
   })
 
   describe('generic error', () => {
     test('returns status code 500 with error message Something went wrong', async () => {
+      jest.clearAllMocks()
+
       MockedGetUsersRecipesByUserId.mockRejectedValueOnce(new Error('Undefined binding(s) detected when compiling FIRST.'))
       MockedHandleGetUpdateDeleteRecipes.mockReturnValueOnce({ code: 500, error: 'Something went wrong'})
+
       const response = await request(mockedServer).get('/getUserRecipes/1')
       expect(response.statusCode).toBe(500)
       expect(response.body.error).toBe('Something went wrong')
+      expect(MockedGetUsersRecipesByUserId).toHaveBeenCalledTimes(1)
+      expect(MockedHandleGetUpdateDeleteRecipes).toHaveBeenCalledTimes(1)
     })
   })
 })
@@ -142,6 +289,17 @@ describe('GET /getRecipe/:recipeId', () => {
   describe('valid request data', () => {
     beforeAll(() => {
       MockedGetRecipeById.mockResolvedValue(CaramelSliceJSONRecipeWithIds)
+    })
+
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    test('Functions are called correctly', async () => {
+      await request(mockedServer).get('/getRecipe/1')
+      expect(MockedGetRecipeById).toHaveBeenCalledTimes(1)
+      expect(MockedGetRecipeById.mock.calls[0][0]).toBe(1)
     })
 
     test('returns status code 200', async () => {
@@ -157,27 +315,37 @@ describe('GET /getRecipe/:recipeId', () => {
     test('body contains recipe', async () => {
       const response = await request(mockedServer).get('/getRecipe/1')
       expect(response.body.recipe).toEqual(CaramelSliceJSONRecipeWithIds)
-      expect(MockedGetUsersRecipesByUserId.mock.calls[0][0]).toBe(1)
+      expect(MockedGetRecipeById.mock.calls[0][0]).toBe(1)
     })
   })
 
   describe('invalid request data', () => {
     test('returns status code 400 with error message Wrong id', async () => {
+      jest.clearAllMocks()
+      
       MockedGetRecipeById.mockRejectedValueOnce(new GetDBError('recipes'))
       MockedHandleGetUpdateDeleteRecipes.mockReturnValueOnce({ code: 400, error: `GetError: Wrong 'recipes' id`})
+      
       const response = await request(mockedServer).get('/getRecipe/1')
       expect(response.statusCode).toBe(400)
       expect(response.body.error).toBe(`GetError: Wrong 'recipes' id`)
+      expect(MockedGetRecipeById).toHaveBeenCalledTimes(1)
+      expect(MockedHandleGetUpdateDeleteRecipes).toHaveBeenCalledTimes(1)
     })
   })
 
   describe('generic error', () => {
     test('returns status code 500 with error message Something went wrong', async () => {
+      jest.clearAllMocks()
+      
       MockedGetRecipeById.mockRejectedValueOnce(new Error('Undefined binding(s) detected when compiling FIRST.'))
       MockedHandleGetUpdateDeleteRecipes.mockReturnValueOnce({ code: 500, error: 'Something went wrong'})
+      
       const response = await request(mockedServer).get('/getRecipe/1')
       expect(response.statusCode).toBe(500)
       expect(response.body.error).toBe('Something went wrong')
+      expect(MockedGetRecipeById).toHaveBeenCalledTimes(1)
+      expect(MockedHandleGetUpdateDeleteRecipes).toHaveBeenCalledTimes(1)
     })
   })
 })
@@ -188,39 +356,94 @@ describe('PATCH /edit', () => {
       MockedUpdateRecipe.mockResolvedValue(1)
     })
 
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    test('Functions are called correctly', async () => {
+      await request(mockedServer).patch('/edit/1').send({ editedRecipe: { cookingTime: { quantity: 40 } } })
+      expect(MockedUpdateRecipe).toHaveBeenCalledTimes(1)
+      expect(MockedUpdateRecipe.mock.calls[0][0]).toBe(1)
+      expect(MockedUpdateRecipe.mock.calls[0][1]).toEqual({ cookingTime: { quantity: 40 } })
+    })
+
     test('returns status code 200', async () => {
-      const response = await request(mockedServer).patch('/edit/1').send({editRecipe: { cookingTime: { quantity: 40 } } })
+      const response = await request(mockedServer).patch('/edit/1').send({ editedRecipe: { cookingTime: { quantity: 40 } } })
       expect(response.statusCode).toBe(200)
     })
 
     test('header specifies content type json', async () => {
-      const response = await request(mockedServer).patch('/edit/1').send({editRecipe: { cookingTime: { quantity: 40 } } })
+      const response = await request(mockedServer).patch('/edit/1').send({ editedRecipe: { cookingTime: { quantity: 40 } } })
       expect(response.headers['content-type']).toContain('json')
     })
   
     test('body contains edited', async () => {
-      const response = await request(mockedServer).patch('/edit/1').send({editRecipe: { cookingTime: { quantity: 40 } } })
+      const response = await request(mockedServer).patch('/edit/1').send({ editedRecipe: { cookingTime: { quantity: 40 } } })
       expect(response.body.edited).toBe(true)
     })
   })
 
   describe('invalid request data', () => {
+    describe('no body.editedRecipe', () => {
+      beforeAll(() => {
+        MockedUpdateRecipe.mockResolvedValue(1)
+      })
+  
+      beforeEach(() => {
+        jest.clearAllMocks()
+      })
+  
+      test('Functions are called correctly', async () => {
+        await request(mockedServer).patch('/edit/1').send()
+        expect(MockedUpdateRecipe).toHaveBeenCalledTimes(0)
+      })
+  
+      test('returns status code 200', async () => {
+        const response = await request(mockedServer).patch('/edit/1').send()
+        expect(response.statusCode).toBe(400)
+      })
+  
+      test('header specifies content type json', async () => {
+        const response = await request(mockedServer).patch('/edit/1').send()
+        expect(response.headers['content-type']).toContain('json')
+      })
+    
+      test('body contains edited', async () => {
+        const response = await request(mockedServer).patch('/edit/1').send()
+        expect(response.body.error).toBe('No edited recipe')
+      })
+    })
+
     test('returns status code 400 with error message Wrong id', async () => {
+      jest.clearAllMocks()
+
       MockedUpdateRecipe.mockRejectedValueOnce( new UpdateDBError())
       MockedHandleGetUpdateDeleteRecipes.mockReturnValueOnce({ code: 400, error: 'UpdateError: Wrong id'})
-      const response = await request(mockedServer).patch('/edit/1').send({editRecipe: { cooking_time: { quantity: null } } })
+
+      const response = await request(mockedServer).patch('/edit/1').send({ editedRecipe: { cooking_time: { quantity: null } } })
+     
       expect(response.statusCode).toBe(400)
       expect(response.body.error).toBe('UpdateError: Wrong id')
+
+      expect(MockedUpdateRecipe).toHaveBeenCalledTimes(1)
+      expect(MockedHandleGetUpdateDeleteRecipes).toHaveBeenCalledTimes(1)
     })
   })
 
   describe('generic error', () => {
     test('returns status code 500 with error message Something went wrong', async () => {
+      jest.clearAllMocks()
+
       MockedUpdateRecipe.mockRejectedValueOnce( new Error('Undefined binding(s) detected when compiling FIRST.'))
       MockedHandleGetUpdateDeleteRecipes.mockReturnValueOnce({ code: 500, error: 'Something went wrong'})
-      const response = await request(mockedServer).patch('/edit/1').send({editRecipe: { cooking_time: { quantity: null } } })
+      
+      const response = await request(mockedServer).patch('/edit/1').send({ editedRecipe: { cooking_time: { quantity: null } } })
+      
       expect(response.statusCode).toBe(500)
       expect(response.body.error).toBe('Something went wrong')
+
+      expect(MockedUpdateRecipe).toHaveBeenCalledTimes(1)
+      expect(MockedHandleGetUpdateDeleteRecipes).toHaveBeenCalledTimes(1)
     })
   })
 })
@@ -229,6 +452,17 @@ describe('DELETE /unsave', () => {
   describe('valid request data', () => {
     beforeAll(() => {
       MockedDeleteItemBySelector.mockResolvedValue(1)
+    })
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    test('Functions are called correctly', async () => {
+      await request(mockedServer).delete('/unsave/1').send({ userId: 1 })
+      expect(MockedDeleteItemBySelector).toHaveBeenCalledTimes(1)
+      expect(MockedDeleteItemBySelector.mock.calls[0][0]).toBe('user_recipes')
+      expect(MockedDeleteItemBySelector.mock.calls[0][1]).toEqual({ user_id: 1, recipe_id: 1 })
     })
 
     test('returns status code 200', async () => {
@@ -248,22 +482,64 @@ describe('DELETE /unsave', () => {
   })
 
   describe('invalid request data', () => {
+    describe('no body.userId', () => {
+      beforeAll(() => {
+        MockedDeleteItemBySelector.mockResolvedValue(1)
+      })
+  
+      beforeEach(() => {
+        jest.clearAllMocks()
+      })
+  
+      test('Functions are called correctly', async () => {
+        await request(mockedServer).delete('/unsave/1').send()
+        expect(MockedDeleteItemBySelector).toHaveBeenCalledTimes(0)
+      })
+  
+      test('returns status code 200', async () => {
+        const response = await request(mockedServer).delete('/unsave/1').send()
+        expect(response.statusCode).toBe(400)
+      })
+  
+      test('header specifies content type json', async () => {
+        const response = await request(mockedServer).delete('/unsave/1').send()
+        expect(response.headers['content-type']).toContain('json')
+      })
+    
+      test('body contains edited', async () => {
+        const response = await request(mockedServer).delete('/unsave/1').send()
+        expect(response.body.error).toBe('No userId')
+      })
+    })
+
     test('returns status code 400 with error message Wrong id', async () => {
+      jest.clearAllMocks()
+      
       MockedDeleteItemBySelector.mockRejectedValueOnce( new DeletionDBError())
       MockedHandleGetUpdateDeleteRecipes.mockReturnValueOnce({ code: 400, error: 'DeletionError: Wrong id' })
+      
       const response = await request(mockedServer).delete('/unsave/1').send({ userId: 1 })
+      
       expect(response.statusCode).toBe(400)
       expect(response.body.error).toBe('DeletionError: Wrong id')
+      expect(MockedDeleteItemBySelector).toHaveBeenCalledTimes(1)
+      expect(MockedHandleGetUpdateDeleteRecipes).toHaveBeenCalledTimes(1)
     })
   })
 
   describe('generic error', () => {
     test('returns status code 500 with error message Something went wrong', async () => {
+      jest.clearAllMocks()
+      
       MockedDeleteItemBySelector.mockRejectedValueOnce( new Error('Undefined binding(s) detected when compiling FIRST.'))
       MockedHandleGetUpdateDeleteRecipes.mockReturnValueOnce({ code: 500, error: 'Something went wrong'})
+      
       const response = await request(mockedServer).delete('/unsave/1').send({ userId: 1 })
+      
       expect(response.statusCode).toBe(500)
       expect(response.body.error).toBe('Something went wrong')
+      expect(MockedDeleteItemBySelector).toHaveBeenCalledTimes(1)
+      expect(MockedHandleGetUpdateDeleteRecipes).toHaveBeenCalledTimes(1)
     })
   })
 })
