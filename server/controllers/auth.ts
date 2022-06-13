e9e3fc10159b72056d86767f87a17f6228e2503b
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-06-14 11:31:16 +1200

    prettify

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,29 +1,35 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 
-import { addItemToDatabase, deleteItemBySelector, updateItemBySelector } from "../db/functions/basicCrud";
-import { changeUserPassword, getUserByEmail } from "../db/functions/users";
+import {
+  addItemToDatabase,
+  deleteItemBySelector,
+  updateItemBySelector,
+} from "../db/functions/basicCrud"
+import { changeUserPassword, getUserByEmail } from "../db/functions/users"
 
-import { comparePasswords, hashPassword } from "../functions/passwords";
-import { createToken } from "../functions/jwt";
-import { createUserDatabaseObject } from "../functions/createDatabaseObjects";
-import { handleLoginErrors, handleRegistrationErrors } from "../functions/errorHandlers";
+import { comparePasswords, hashPassword } from "../functions/passwords"
+import { createToken } from "../functions/jwt"
+import { createUserDatabaseObject } from "../functions/createDatabaseObjects"
+import {
+  handleLoginErrors,
+  handleRegistrationErrors,
+} from "../functions/errorHandlers"
 
-import { IncomingUser } from "../types/User";
+import { IncomingUser } from "../types/User"
 
 const maxAge = 7 * 24 * 60 * 60 * 1000 // 7 days
 
 export const registerUser = async (req: Request, res: Response) => {
   const user = req.body.user as IncomingUser
 
-  
   try {
     const password = await hashPassword(user.password)
     const userDatabaseObject = createUserDatabaseObject(user, password)
-    
-    const newUserId = await addItemToDatabase('users', userDatabaseObject)
+
+    const newUserId = await addItemToDatabase("users", userDatabaseObject)
     const token = createToken(newUserId)
 
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge } )
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge })
     res.status(201).json({ id: newUserId })
   } catch (err: unknown) {
     const { code, error } = handleRegistrationErrors(err)
@@ -41,14 +47,14 @@ export const loginUser = async (req: Request, res: Response) => {
     if (passwordsMatch) {
       const token = createToken(user.id)
 
-      res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge } )
+      res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge })
       res.json({ id: user.id })
     } else {
-      throw new Error('Wrong password')
+      throw new Error("Wrong password")
     }
-  } catch(err: unknown) {
+  } catch (err: unknown) {
     const { code, error } = handleLoginErrors(err)
-    return res.status(code).json({error})
+    return res.status(code).json({ error })
   }
 }
 
@@ -57,22 +63,24 @@ export const changePassword = async (req: Request, res: Response) => {
 
   try {
     const user = await getUserByEmail(email)
-    const passwordsMatch = await comparePasswords(oldPassword, user.password_hash)
-    
+    const passwordsMatch = await comparePasswords(
+      oldPassword,
+      user.password_hash
+    )
+
     if (passwordsMatch) {
       const hashNewPassword = await hashPassword(newPassword)
       await changeUserPassword(email, hashNewPassword)
 
       res.json({ passwordChanged: true })
     } else {
-      throw new Error('Wrong password')
+      throw new Error("Wrong password")
     }
-
-  } catch(err: unknown) {
+  } catch (err: unknown) {
     const { code, error } = handleLoginErrors(err)
     return res.status(code).json({
       error,
-      passwordChanged: false
+      passwordChanged: false,
     })
   }
 }
@@ -84,27 +92,30 @@ export const updateUser = async (req: Request, res: Response) => {
 
   try {
     const dbUser = await getUserByEmail(email)
-    const passwordsMatch = await comparePasswords(password, dbUser.password_hash)
+    const passwordsMatch = await comparePasswords(
+      password,
+      dbUser.password_hash
+    )
 
     if (passwordsMatch) {
       const editedUserDatabaseObject = createUserDatabaseObject(editedUser)
-      await updateItemBySelector('users', { email }, editedUserDatabaseObject)
+      await updateItemBySelector("users", { email }, editedUserDatabaseObject)
 
       return res.json({ userUpdated: true })
     } else {
-      throw new Error('Wrong password')
+      throw new Error("Wrong password")
     }
-  } catch(err: unknown) {
+  } catch (err: unknown) {
     const { code, error } = handleLoginErrors(err)
     return res.status(code).json({
       error,
-      userUpdated: false
+      userUpdated: false,
     })
   }
 }
 
 export const logoutUser = async (req: Request, res: Response) => {
-  res.cookie('jwt', '', { httpOnly: true, maxAge: 1 } )
+  res.cookie("jwt", "", { httpOnly: true, maxAge: 1 })
   res.json({ id: null })
 }
 
@@ -116,15 +127,14 @@ export const deleteUser = async (req: Request, res: Response) => {
     const passwordsMatch = await comparePasswords(password, user.password_hash)
 
     if (passwordsMatch) {
-      await deleteItemBySelector('users', { email })
-      res.cookie('jwt', '', { httpOnly: true, maxAge: 1 } )
+      await deleteItemBySelector("users", { email })
+      res.cookie("jwt", "", { httpOnly: true, maxAge: 1 })
       res.json({ deleted: true })
-
     } else {
-      throw new Error('Wrong password')
+      throw new Error("Wrong password")
     }
-  } catch(err: unknown) {
+  } catch (err: unknown) {
     const { code, error } = handleLoginErrors(err)
-    return res.status(code).json({error})
+    return res.status(code).json({ error })
   }
 }
--- server/controllers/recipe.ts
+++ server/controllers/recipe.ts
@@ -1,23 +1,29 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 
-import { deleteItemBySelector } from "../db/functions/basicCrud";
-import { addRecipe, getRecipeById, getUsersRecipesByUserId, updateRecipe, userSavesRecipe } from "../db/functions/recipe";
+import { deleteItemBySelector } from "../db/functions/basicCrud"
+import {
+  addRecipe,
+  getRecipeById,
+  getUsersRecipesByUserId,
+  updateRecipe,
+  userSavesRecipe,
+} from "../db/functions/recipe"
 
-import { handleGetUpdateDeleteRecipes } from "../functions/errorHandlers";
+import { handleGetUpdateDeleteRecipes } from "../functions/errorHandlers"
 
-import { JSONRecipe } from "../types/JSONRecipe";
+import { JSONRecipe } from "../types/JSONRecipe"
 
-export const addNewRecipe = async (req: Request, res: Response) => { 
+export const addNewRecipe = async (req: Request, res: Response) => {
   try {
     const jsonRecipe: JSONRecipe = req.body.recipe
     const userId = req.body.userId
-  
-    if (userId === undefined) { 
-      return res.status(400).json({error: 'No userId'}) 
+
+    if (userId === undefined) {
+      return res.status(400).json({ error: "No userId" })
     }
 
-    if(jsonRecipe === undefined) {
-      return res.status(400).json({error: 'No jsonRecipe'}) 
+    if (jsonRecipe === undefined) {
+      return res.status(400).json({ error: "No jsonRecipe" })
     }
 
     // add recipe and then add ingredients if needed
@@ -28,25 +34,24 @@ export const addNewRecipe = async (req: Request, res: Response) => {
 
     return res.status(201).json({ id: recipeId })
   } catch (e: unknown) {
-    return res.status(500).json({error: 'Something went wrong'})
+    return res.status(500).json({ error: "Something went wrong" })
   }
-  
 }
 
 export const saveRecipe = async (req: Request, res: Response) => {
   try {
     const recipeId = parseInt(req.params.recipeId)
     const userId: number = req.body.userId
-  
-    if (userId === undefined) { 
-      return res.status(400).json({error: 'No userId'}) 
-    } 
+
+    if (userId === undefined) {
+      return res.status(400).json({ error: "No userId" })
+    }
 
     const id = await userSavesRecipe(userId, recipeId)
 
-    return res.status(201).json({id})
+    return res.status(201).json({ id })
   } catch (e: unknown) {
-   return res.status(500).json({error: 'Something went wrong'})
+    return res.status(500).json({ error: "Something went wrong" })
   }
 }
 
@@ -57,8 +62,8 @@ export const getUserRecipes = async (req: Request, res: Response) => {
     const recipes = await getUsersRecipesByUserId(userId)
 
     return res.json({ recipes })
-  } catch(e) {
-    const {code, error} = handleGetUpdateDeleteRecipes(e)
+  } catch (e) {
+    const { code, error } = handleGetUpdateDeleteRecipes(e)
     return res.status(code).json({ error })
   }
 }
@@ -70,27 +75,27 @@ export const getRecipe = async (req: Request, res: Response) => {
     const recipe = await getRecipeById(recipeId)
 
     return res.json({ recipe })
-  } catch(e) {
-    const {code, error} = handleGetUpdateDeleteRecipes(e)
+  } catch (e) {
+    const { code, error } = handleGetUpdateDeleteRecipes(e)
     return res.status(code).json({ error })
   }
 }
 
-export const editRecipe = async (req: Request, res: Response) => {  
+export const editRecipe = async (req: Request, res: Response) => {
   try {
     const recipeId = parseInt(req.params.recipeId)
     const editedRecipe = req.body.editedRecipe as Partial<JSONRecipe>
-  
-    if (editedRecipe === undefined) { 
-      return res.status(400).json({error: 'No edited recipe'}) 
+
+    if (editedRecipe === undefined) {
+      return res.status(400).json({ error: "No edited recipe" })
     }
 
     // update the recipe
     await updateRecipe(recipeId, editedRecipe)
 
-    return res.json({ edited: true})
-  } catch(e: unknown) {
-    const {code, error} = handleGetUpdateDeleteRecipes(e)
+    return res.json({ edited: true })
+  } catch (e: unknown) {
+    const { code, error } = handleGetUpdateDeleteRecipes(e)
     return res.status(code).json({ edited: false, error })
   }
 }
@@ -100,16 +105,16 @@ export const unsaveRecipe = async (req: Request, res: Response) => {
     const recipe_id = parseInt(req.params.recipeId)
     const user_id = req.body.userId
 
-    if (user_id === undefined) { 
-      return res.status(400).json({error: 'No userId'}) 
+    if (user_id === undefined) {
+      return res.status(400).json({ error: "No userId" })
     }
 
     // delete the recipe user connection for given user
-    await deleteItemBySelector('user_recipes', {user_id, recipe_id})
-    
-    return res.json({unsaved: true})
-  } catch(e) {
-    const {code, error} = handleGetUpdateDeleteRecipes(e)
+    await deleteItemBySelector("user_recipes", { user_id, recipe_id })
+
+    return res.json({ unsaved: true })
+  } catch (e) {
+    const { code, error } = handleGetUpdateDeleteRecipes(e)
     return res.status(code).json({ error })
   }
 }
