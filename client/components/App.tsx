22bdc6d54bccdbe69503c6c59fd0b0bffeeacc27
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-06-28 13:58:21 +1200

    add mui

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,18 @@
+import { Card, CardContent, Typography } from "@mui/material"
 import React from "react"
 
 const App: React.FC = () => {
-  return <h1>App has arrived</h1>
+  return (
+    <Card sx={{ maxWidth: 275 }}>
+      <CardContent>
+        <Typography 
+          variant="h1"
+        >
+          Hello World
+        </Typography>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default App
